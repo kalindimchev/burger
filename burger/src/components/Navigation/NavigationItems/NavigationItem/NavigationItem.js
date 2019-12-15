729d13845d49943931bb1aac6271b04444b2982b
kalindimchev <kalin.dimchev@gmail.com>
2019-12-16 00:30:41 +0200

    Add Hamburger icon

--- burger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,7 @@ import classes from './NavigationItem.css';
 const navigationItem = props => {
     return (
         <li className={classes.NavigationItem}>
-            <a href={props.link} clasName={props.active ? classes.active : null}>{props.children}</a>
+            <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
         </li>
     );
 }
